6b89ca9c278ce287aacf229997e2ba84ce816d97
Luis Fernando Farji <fernando.farji@gmail.com>
2022-01-19 11:27:25 -0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.header .search-form');
+
+searchBtn.onclick = () => {
+  searchBtn.classList.toggle('fa-times');
+  searchForm.classList.toggle('active');
+}
